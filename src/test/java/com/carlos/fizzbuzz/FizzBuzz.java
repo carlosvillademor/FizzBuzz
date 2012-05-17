669aeff61f9ad0e1b966b69b5cbd9ffca0aaf366
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2012-05-17 07:03:41 +0100

    Printing Fizz for numbers multiple of 3

--- src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
+++ src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package com.carlos.fizzbuzz;
 public class FizzBuzz {
 
     public String write(Integer number) {
-        if (number == 3) {
+        if (number%3 == 0) {
             return "Fizz";
         }
         return number.toString();
