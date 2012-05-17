5119fcd09d532436c7644933fb1d18b56c17c13e
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2012-05-17 07:16:35 +0100

    Extracting common logic of whether a number is multiple of another

--- src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
+++ src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
@@ -3,21 +3,17 @@ package com.carlos.fizzbuzz;
 public class FizzBuzz {
 
     public String write(Integer number) {
-        if (isMultipleOf3(number)) {
+        if (isMultipleOf(number, 3)) {
             return "Fizz";
         }
-        if (isMultipleOf5(number)) {
+        if (isMultipleOf(number, 5)) {
             return "Buzz";
         }
         return number.toString();
     }
 
-    private boolean isMultipleOf5(Integer number) {
-        return number%5 == 0;
-    }
-
-    private boolean isMultipleOf3(Integer number) {
-        return number%3 == 0;
+    private boolean isMultipleOf(Integer number, Integer multipleOf) {
+        return number%multipleOf == 0;
     }
 
 }
