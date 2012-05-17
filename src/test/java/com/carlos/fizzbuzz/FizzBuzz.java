5561c239fdce56c8cd98c6baf9e5a3c3aa6368eb
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2012-05-17 07:32:32 +0100

    Printing numbers from 1 to 100 translated with FizzBuzz rules

--- src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
+++ src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,15 @@ package com.carlos.fizzbuzz;
 
 public class FizzBuzz {
 
+    public String play() {
+        String result = "";
+        for (int x = 1; x <= 100; x++) {
+            result += write(x) + " ";
+        }
+        System.out.println(result);
+        return result;
+    }
+
     public String write(Integer number) {
         String result = "";
         if (isMultipleOf(number, 3)) {
--- src/test/java/com/carlos/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/carlos/fizzbuzz/FizzBuzzTest.java
@@ -87,4 +87,13 @@ public class FizzBuzzTest {
         assertThat("The result is incorrect", result, is("FizzBuzz"));
     }
 
+    @Test
+    public void when_fizzbuzz_play_then_numbers_from_1_to_100_translated_with_FizzBuzz_rules_should_be_printed() {
+        //When
+        String result = fizzbuzz.play();
+
+        //Then
+        assertThat("The result is incorrect", result, is("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz 31 32 Fizz 34 Buzz Fizz 37 38 Fizz Buzz 41 Fizz 43 44 FizzBuzz 46 47 Fizz 49 Buzz Fizz 52 53 Fizz Buzz 56 Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz Buzz 71 Fizz 73 74 FizzBuzz 76 77 Fizz 79 Buzz Fizz 82 83 Fizz Buzz 86 Fizz 88 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz "));
+    }
+
 }
