d55fac054c05fd2019d875170b101137c69384c3
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2012-05-17 07:11:02 +0100

    Extracting logic of whether the number is multiple of 3 to a method

--- src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
+++ src/test/java/com/carlos/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,14 @@ package com.carlos.fizzbuzz;
 public class FizzBuzz {
 
     public String write(Integer number) {
-        if (number%3 == 0) {
+        if (isMultipleOf3(number)) {
             return "Fizz";
         }
         return number.toString();
+    }
+
+    private boolean isMultipleOf3(Integer number) {
+        return number%3 == 0;
     }
 
 }
